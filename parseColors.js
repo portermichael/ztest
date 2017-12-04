5c8e70d4e0fb3e8032a1951abbadcfc13a4adcd4
mikeporter <porter.michael.james@gmail.com>
2017-12-03 20:49:36 -0800

    wrote some tests

--- parseColors.js
+++ parseColors.js
@@ -1,72 +1,58 @@
 // Write a function that accepts a string as input and returns the color that it represents as a decimal expression of the binary where it goes rgb.
 
 function cssStringToColor (colorString) {
-
+  console.log(colorString);
   function validateHex (stringArray) {
     // Check hex length, check starts with '#' & check that every digit is a hex
     // this could be written with fewer lines, combining if statements into more complex logic, but would be harder to interpret.
+    // We could convert our character values (a-f) to decimal numbers (10-15) and drop the '#' after validation, but we want validateHex to do one specific action. We want it to run first so that we fail fast too.
     let stringLength = stringArray.length;
     if (!(stringLength === 4 || stringLength === 7)) {
-      return 'error!';
+      throw new Error ('String must begin with # and  be a valid hexadecimal with total length 4 or 7.')
     } else if (stringArray[0] !== '#') {
-      return 'error!';
+      throw new Error ('Begin string with #.')
     }
     for (let i = 1; i < stringLength; i++) {
-      // we could convert our character values (a-f) to decimal numbers (10-15) here, but we want validateHex to do one specific action.
-      if (!(0 <= stringArray[i] <= 9) || !(stringArray[i] >= 'a' && stringArray[i] <= 'f')) {
-        return 'error!';
+      if (!(stringArray[i] >= 0 && stringArray[i] <= 9) && !(stringArray[i] >= 'a' && stringArray[i] <= 'f')) {
+        throw new Error ('String is not hexadecimal.')
       }
     }
   }
   function threeToSixHex (strArray) {
     // this takes an array with elements like #f00 and converts it to #ff0000
     let result = strArray[0];
-    for(let i = 1; i < strArray.length; i++) {
+    for (let i = 1; i < strArray.length; i++) {
       result = result.concat(strArray[i]).concat(strArray[i]);
     }
     return result;
   };
 
-  function swapRGBPositionsDropHash (arrayOfChars) {
-    // #ff0000 is written in order RGB, but we want it in order BGR.
-
+  function prepStringToColorArr (arrayOfChars) {
+    // #ff0000 is written in order RGB, but we want it in order BGR when we get to binary.
+    // We could call .shift() but I don't want to manipulate the input.
+    let result = [];
+    for (let i = arrayOfChars.length - 1; i !== 1; i = i-2) {
+      result.push(arrayOfChars[i-1]);
+      result.push(arrayOfChars[i]);
+    }
+    return result;
   }
 
   function convertCharToInteger (arrayOfChars) {
-    // unicode elements?
+    // I don't know of a great way to get js to use hex instead of base 10, so we need to do some kind of converstion. I first used a clunky switch statement and though using unicode would be doing the same thing. It is essentially doing the same thing, but now I've got it all in one line.
     let convertHexChars = [];
-    arrayOfChars.map((element) =>
-    { if(0 <= element <= 9) {
-        convertHexChars.push(element)
-        return;
+    arrayOfChars.map((element) => {
+      if (0 <= element <= 9) {
+        convertHexChars.push(element);
       } else {
-        switch (element) {
-          case 'a':
-            convertHexChars.push(10)
-            break;
-          case 'b':
-            convertHexChars.push(11)
-            break;
-          case 'c':
-            convertHexChars.push(12)
-            break;
-          case 'd':
-            convertHexChars.push(13)
-            break;
-          case 'e':
-            convertHexChars.push(14)
-            break;
-          case 'f':
-            convertHexChars.push(15)
-            break;
-        }
-      }})
+        convertHexChars.push(element.charCodeAt() - 87);
+      }
+    });
   }
 
   function hexToBinary (arrayOfHex) {
-    arrayOfHex.reduce()
-
-    function convertHextoBinary (hexDigit) {
+    function convertHexToBinary (hexDigit) {
+      // This could be done recurisvely, but js isn't really optimized for recursion. I think the clarity of nomenclature and extracting out the helper function makes our code small and clean enough to maintain readability.
       let binaryArray = [0, 0, 0, 0];
       function maintainBase2 (baseTwoArray, position) {
         if (baseTwoArray[position] === 2) {
@@ -74,46 +60,61 @@ function cssStringToColor (colorString) {
           baseTwoArray[position - 1] = baseTwoArray[position - 1]++;
         }
       }
-      // This could be done recurisvely and would look cleaner.
-      while(0 < element <= 15) {
+      while (0 < element <= 15) {
         binaryArray[3] = binaryArray[3] + 1;
-        maintainBase2 (binaryArray, 3);
-        maintainBase2 (binaryArray, 2);
-        maintainBase2 (binaryArray, 1);
-        if (binaryArray[3] === 2) {
-          binaryArray[3] - 1;
-          binaryArray[2] + 1;
-        } else if (binaryArray[2] === 2) {
-          binaryArray[2] - 1;
-          binaryArray[1] + 1;
-        } else if (binaryArray[1] === 2) {
-          binaryArray[1] - 1;
-          binaryArray[0] + 1;
+        for (let i = 3; i !== 0; i--) {
+          maintainBase2 (binaryArray, i)
         }
         element--;
       }
       return binaryArray;
     }
 
+    return arrayOfHex.map(convertHexToBinary(element));
   }
 
   function binaryToBase10 () {
 
   }
-  // We are iterating, inserting and validating elements in our string. I would argue it's easier to interpret and understand the code by just making it an array once, rather than trying to deal with it as a string.
+  // We are iterating, inserting and validating elements in our string. I would argue it's easier to interpret and understand the code by just making it an array once, rather than manipulating it as a string.
   let strSplitLowArray = colorString.toLowerCase().split('');
   validateHex(strSplitLowArray);
   if (strSplitLowArray === 4) {
     strSplitLowArray = threeToSixHex(strSplitLowArray)
   }
-
-
+  // i now have a vaild array of length 7.
+  // strSplitLowArray = prepStringToColorArr(strSplitLowArray);
+  console.log(strSplitLowArray, 'end');
 }
-// 255 -> ff is hex 255, converts to -b- 00000000 -g- 00000000 -r- 11111111 in binary, drop the 0s and convert to dec
-console.log(cssStringToColor('#F00'));
-// 8388736 - > 80 is hex 128, converts to -b- 10000000 -g- 00000000 -r- 10000000
-console.log(cssStringToColor('#800080'));
-// 42495 -> ff is hex 255, a5 is hex 165, converts to -b- 00000000 -g- 10100101 - r- 11111111
-console.log(cssStringToColor('#ffa500'));
+
+
+function testeroonie (str) {
+  try {
+    console.log(cssStringToColor(str));
+    console.log('valid', '************************')
+  } catch (error) {
+    console.log(error.name + ':' + error.message, '************************');
+  }
+}
+// test lower cases so A is a and within range
+testeroonie('#aAf');
+// no hash
+testeroonie('fffF');
+// test wrong length
+testeroonie('#fFff');
+// test upper and lowercase outside of range
+testeroonie('#gff');
+testeroonie('#Gff');
+// test numbers in and outside of range
+testeroonie('#3ff');
+testeroonie('#15ff');
 // error -> no #, can't double,
-console.log(cssStringToCoor('oops'));
+testeroonie('oops');
+// valid
+testeroonie('#fA0000')
+// 255 -> ff is hex 255, converts to -b- 00000000 -g- 00000000 -r- 11111111 in binary, drop the 0s and convert to dec
+testeroonie('#F00')
+// 8388736 - > 80 is hex 128, converts to -b- 10000000 -g- 00000000 -r- 10000000
+testeroonie('#800080')
+// 42495 -> ff is hex 255, a5 is hex 165, converts to -b- 00000000 -g- 10100101 - r- 11111111
+testeroonie('#ffa500')
